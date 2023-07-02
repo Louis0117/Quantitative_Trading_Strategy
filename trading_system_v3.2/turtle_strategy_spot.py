0ef2fe269fac0423424966061eaa3164c94f9237
Louis0117 <welcome870117@gmail.com>
2023-07-03 00:13:25 +0800

    turtle

--- trading_system_v3.2/turtle_strategy_spot.py
+++ trading_system_v3.2/turtle_strategy_spot.py
@@ -28,14 +28,14 @@ BINANCE_SECRET = ''
 LOG_FILE_DIR = '/Users/welcome870117/Desktop/git_project/Quantitative_trading_strategy/trading_system_v3.2/log_file.csv'
 TIME_ZONE = pytz.timezone("utc")
 
-'''
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--symbol', type=str, help ='Symbol')
+parser.add_argument('--asset_value', type=float, help ='ASSET_VALUES')
 args = parser.parse_args()
 SYMBOL = args.symbol
-'''
-SYMBOL = 'ETHUSDT'
-ASSET_VALUES = 200
+ASSET_VALUES = args.asset_value
+
 
 # calculate TR
 def TR_value(df):
@@ -92,8 +92,215 @@ def _calculate_order_units(history_data, account_value):
     units = account_value*0.01/N
     return units
 
+
 # strategy 
 def turtle_strategy(client, symbol, log_file, history_data, current_price):
+    position_units = 0
+    side = 0
+    stop_loss = 0
+    add_position = 0
+    current_spot_usdt_value = 0
+    # read log file
+    for i in range(len(log_file)):
+        if log_file['symbol'][i] == symbol and log_file['strategy'][i] == 'turtle_spot':
+            position_units+=1
+            side = log_file['side'][i]
+            stop_loss = log_file['stop_loss_price'][i]
+            add_position = log_file['add_position_price'][i]
+            current_spot_usdt_value += log_file['USDT_value'][i]
+    
+    print('date:', history_data['timestamp'].iloc[-1])
+    print('position units:', position_units)
+    print('side:', side)
+    print('stop loss:', stop_loss)  
+    print('add position:', add_position)    
+    print('current price:', current_price)
+    print('20day high:', history_data['20day_high'].iloc[-1])
+    print('20day low:', history_data['20day_low'].iloc[-1])
+    print('10day high:', history_data['10day_high'].iloc[-1])
+    print('10day low:', history_data['10day_low'].iloc[-1])
+    
+    remain_position_size = ASSET_VALUES - current_spot_usdt_value
+    print('remain_position_size:', remain_position_size)
+    print('=================================')
+    
+    if position_units == 0:
+        if current_price > history_data['20day_high'].iloc[-1]: 
+            # check account USDT balance 
+            #wallet_USDT = _wallet_USDT(client)
+            # calculate order size
+            units = _calculate_order_units(history_data, ASSET_VALUES)
+            # check account avaliable USDT
+            #available_USDT = _available_USDT(client)
+            available_USDT = float(client.check_spot_asset('USDT'))
+            requiment_USDT = units*current_price
+            print('create long position.....')
+            print('check available USDT balance.....')
+            print('available USDT:', available_USDT)
+            print('USDT requirement:', requiment_USDT)
+            #discreminate USDT is enough or not
+            if requiment_USDT>remain_position_size:
+                print('small than remain ASSET size')
+                return False
+            # discreminate USDT is enough or not
+            if units*current_price>available_USDT:
+                print('USDT is not enough')
+                return False
+            else:
+                # create long position ....
+                #quantity = client.future_perpetual_buy(SYMBOL, SYMBOL[:-4], 'USDT', order_size=0, order_price = None, quantity=units)####
+                quantity = client.spot_buy(symbol, SYMBOL[:-4], 'USDT', order_size = None, quantity = units, order_price = None)
+                if quantity == False:
+                    return False
+                else:
+                    # update log file
+                    #utc_time = datetime.now(TIME_ZONE)
+                    utc_time = history_data['timestamp'].iloc[-1]
+                    N = history_data['ATR_value'].iloc[-1]
+                    stop_loss = current_price-2*N
+                    add_position = current_price+0.5*N
+                    # ['timestamp', 'symbol', 'side', 'strategy', 'quantity', 'add_position_price', 'stop_loss_price'])
+                    transaction_record = {'timestamp':utc_time, 'symbol':symbol, 'side':'BUY', 'strategy':'turtle_spot', 'quantity':quantity, 'USDT_value':requiment_USDT, 'add_position_price':add_position, 'stop_loss_price':stop_loss}
+                    log_file = log_file.append(transaction_record, ignore_index=True)
+                    test_log_file = test_log_file.append(transaction_record, ignore_index=True)
+                    # save file
+                    log_file.to_csv(LOG_FILE_DIR, index=False)
+                    #test_log_file.to_csv(TEST_LILE_DIR, index=False)
+                    #print('quantity:', quantity)
+                    
+    else:
+        if side == 'BUY':
+            # add position 
+            if current_price > add_position and position_units<4:
+                # check account USDT balance 
+                #wallet_USDT = _wallet_USDT(client)
+                # calculate order size
+                units = _calculate_order_units(history_data, ASSET_VALUES)
+                units = units*(1-position_units*0.15)
+                # check account avaliable USDT
+                #available_USDT = _available_USDT(client)
+                available_USDT = float(client.check_spot_asset('USDT'))
+                requiment_USDT = units*current_price
+                print('create long position.....')
+                print('check available USDT balance.....')
+                print('available USDT:', available_USDT)
+                print('USDT requirement:', requiment_USDT)
+                if requiment_USDT>remain_position_size:
+                    print('small than remain ASSET size')
+                    return False
+                
+                # discreminate USDT is enough or not
+                if units*current_price>available_USDT:
+                    print('USDT is not enough')
+                    return False
+                else:
+                    # create long position ....
+                    #quantity = client.future_perpetual_buy(SYMBOL, SYMBOL[:-4], 'USDT', order_size=0, order_price = None, quantity=units)
+                    quantity = client.spot_buy(symbol, SYMBOL[:-4], 'USDT', order_size = None, quantity = units, order_price = None)
+                    if quantity == False:
+                        return False
+                    else:
+                        # update log file
+                        #utc_time = datetime.now(TIME_ZONE)
+                        utc_time = history_data['timestamp'].iloc[-1]
+                        N = history_data['ATR_value'].iloc[-1]
+                        stop_loss = current_price-2*N
+                        add_position = current_price+0.5*N
+                        transaction_record = {'timestamp':utc_time, 'symbol':symbol, 'side':'BUY', 'strategy':'turtle_spot', 'quantity':quantity, 'USDT_value':requiment_USDT, 'add_position_price':add_position, 'stop_loss_price':stop_loss}
+                        log_file = log_file.append(transaction_record, ignore_index=True)
+                        test_log_file = test_log_file.append(transaction_record, ignore_index=True)
+                        # save file
+                        log_file.to_csv(LOG_FILE_DIR, index=False)
+                        #test_log_file.to_csv(TEST_LILE_DIR, index=False)
+                        #print('quantity:', quantity)
+                        
+            elif current_price < stop_loss:
+                #calculate quantity which need trade  
+                sell_quantity = 0
+                for i in range(len(log_file)):
+                    if log_file['symbol'][i] == symbol and log_file['strategy'][i] == 'turtle_spot':
+                        sell_quantity += log_file['quantity'][i]
+                
+                
+                # check spot quantity
+                available_spot = float(client.check_spot_asset(symbol[:-4]))
+                available_spot_USDT_value = available_spot*current_price
+                print('available_spot_USDT_value:', available_spot_USDT_value)
+                
+                if available_spot_USDT_value<11:
+                    for i in range(len(log_file)):
+                        if log_file['symbol'][i] == symbol and log_file['strategy'][i] == 'turtle_spot':
+                            log_file = log_file.drop(i, axis=0, inplace=False) 
+                    log_file.reset_index(inplace=True, drop=True) 
+                    # save file
+                    log_file.to_csv(LOG_FILE_DIR, index=False)
+                    return True
+                    
+                # close position   
+                elif available_spot >= sell_quantity:
+                    quantity = client.spot_sell(symbol, 'USDT', SYMBOL[:-4], order_size = None, quantity = sell_quantity, order_price = None)
+                    if quantity != False:    
+                        for i in range(len(log_file)):
+                            if log_file['symbol'][i] == symbol and log_file['strategy'][i] == 'turtle_spot':
+                                log_file = log_file.drop(i, axis=0, inplace=False) 
+                        log_file.reset_index(inplace=True, drop=True) 
+                        # save file
+                        log_file.to_csv(LOG_FILE_DIR, index=False)
+                        
+                else:
+                    quantity = client.spot_sell(symbol, 'USDT', SYMBOL[:-4], order_size = None, quantity = available_spot, order_price = None)
+                    if quantity != False:    
+                        for i in range(len(log_file)):
+                            if log_file['symbol'][i] == symbol and log_file['strategy'][i] == 'turtle_spot':
+                                log_file = log_file.drop(i, axis=0, inplace=False) 
+                        log_file.reset_index(inplace=True, drop=True) 
+                        # save file
+                        log_file.to_csv(LOG_FILE_DIR, index=False)
+                
+                
+            elif current_price < history_data['10day_low'].iloc[-1]:
+                #calculate quantity which need trade  
+                sell_quantity = 0
+
+                for i in range(len(log_file)):
+                    if log_file['symbol'][i] == symbol and log_file['strategy'][i] == 'turtle_spot':
+                        sell_quantity += log_file['quantity'][i]
+                                
+                # check spot quantity
+                available_spot = float(client.check_spot_asset(symbol[:-4]))
+                available_spot_USDT_value = available_spot*current_price
+                print('available_spot_USDT_value:', available_spot_USDT_value)
+                # close position   
+                if available_spot_USDT_value<11:
+                    for i in range(len(log_file)):
+                        if log_file['symbol'][i] == symbol and log_file['strategy'][i] == 'turtle_spot':
+                            log_file = log_file.drop(i, axis=0, inplace=False) 
+                    log_file.reset_index(inplace=True, drop=True) 
+                    # save file
+                    log_file.to_csv(LOG_FILE_DIR, index=False)
+                    return True
+                
+                elif available_spot >= sell_quantity:
+                    quantity = client.spot_sell(symbol, 'USDT', SYMBOL[:-4], order_size = None, quantity = sell_quantity, order_price = None)
+                    if quantity != False:
+                        for i in range(len(log_file)):
+                            if log_file['symbol'][i] == symbol and log_file['strategy'][i] == 'turtle_spot':
+                                log_file = log_file.drop(i, axis=0, inplace=False) 
+                        log_file.reset_index(inplace=True, drop=True) 
+                        # save file
+                        log_file.to_csv(LOG_FILE_DIR, index=False)
+                    
+                else:
+                    quantity = client.spot_sell(symbol, 'USDT', SYMBOL[:-4], order_size = None, quantity = available_spot, order_price = None)
+                    if quantity != False:
+                        for i in range(len(log_file)):
+                            if log_file['symbol'][i] == symbol and log_file['strategy'][i] == 'turtle_spot':
+                                log_file = log_file.drop(i, axis=0, inplace=False) 
+                        log_file.reset_index(inplace=True, drop=True) 
+                        # save file
+                        log_file.to_csv(LOG_FILE_DIR, index=False)
+# strategy 
+def turtle_strategy__(client, symbol, log_file, history_data, current_price):
     position_units = 0
     side = 0
     stop_loss = 0
